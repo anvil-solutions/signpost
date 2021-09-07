e61085d4c8867650308838b0b9cbdc378f079bb1
Domi04151309 <36272047+Domi04151309@users.noreply.github.com>
2021-09-07 22:57:22 +0200

    Better UI

--- index.php
+++ index.php
@@ -4,8 +4,51 @@
   <p>
     Signpost is a web app for checking basic website performance. Enter an web page url below to start testing.
   </p>
-  <form method="POST" action="./check">
+  <form id="form" method="POST" action="./check">
     <label><div>URL: </div><input name="url" type="text" required></label>
     <div class="btnContainer"><button type="submit" class="btn">Check Now</button></div>
   </form>
+  <div id="result" style="display:none">
+    <h2>Result</h2>
+    <p>
+      Below you can see the results of the performed tests.
+    </p>
+    <h3>Failed Tests</h3>
+    <ul id="failed"></ul>
+    <h3>Passed Tests</h3>
+    <ul id="passed"></ul>
+  </div>
 </main>
+<script>
+  const formElement = document.getElementById('form');
+  const resultElement = document.getElementById('result');
+  const failedElement = document.getElementById('failed');
+  const passedElement = document.getElementById('passed');
+  formElement.addEventListener('submit', e => {
+    e.preventDefault();
+    const formData = new FormData(formElement);
+    fetch('./check', {
+      method: 'POST',
+      body: formData,
+    })
+    .then(response => response.json())
+    .then(result => {
+      resultElement.style.display = 'block';
+      failedElement.innerHTML = '';
+      passedElement.innerHTML = '';
+      for (let i = 0; i < result.failed.length; i++) {
+        const listItem = document.createElement('li');
+        listItem.append(document.createTextNode(result.failed[i]));
+        failedElement.appendChild(listItem);
+      }
+      for (let i = 0; i < result.passed.length; i++) {
+        const listItem = document.createElement('li');
+        listItem.append(document.createTextNode(result.passed[i]));
+        passedElement.appendChild(listItem);
+      }
+    })
+    .catch(error => {
+      resultElement.style.display = 'none';
+    });
+  }, false);
+</script>
