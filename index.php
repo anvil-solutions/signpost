590734143cdd17ac824eec30074f733017527ebe
Domi04151309 <36272047+Domi04151309@users.noreply.github.com>
2021-09-09 21:26:10 +0200

    Update design

--- index.php
+++ index.php
@@ -1,24 +1,27 @@
 <?php require_once('./src/layout/header.php'); ?>
-<main class="welcome">
+<main>
   <h2>Welcome to Signpost</h2>
   <p>
     Signpost is a web app for checking basic website performance. Enter an web page url below to start testing.
   </p>
   <form id="form" method="POST" action="./check">
-    <label><div>URL: </div><input name="url" type="text" required></label>
-    <div class="btnContainer"><button type="submit" class="btn">Check Now</button></div>
+    <input id="input" aria-label="Web Page URL" name="url" type="text" required>
+    <button type="submit" class="btn">Check Now</button>
   </form>
   <div id="result" style="display:none">
-    <h2>Results for "<span id="url"><span>"</h2>
+    <h2>Results</h2>
     <p>
-      Below you can see the results of the performed tests.
+      Below you can view the results of the performed tests.
     </p>
-    <h3>Score</h3>
-    <p>The site has achieved a score of <span id="score"></span>/100</p>
-    <h3>Failed Tests</h3>
-    <ul id="failed"></ul>
-    <h3>Passed Tests</h3>
-    <ul id="passed"></ul>
+    <p>The site has achieved a score of <span id="score"></span>/100.</p>
+    <details open>
+      <summary><h3>Failed Tests <small class="indicator"></small></h3></summary>
+      <ul id="failed"></ul>
+    </details>
+    <details>
+      <summary><h3>Passed Tests <small class="indicator"></small></h3></summary>
+      <ul id="passed"></ul>
+    </details>
   </div>
   <div id="error" style="display:none">
     <h2>Test Failed</h2>
@@ -29,12 +32,13 @@
 </main>
 <script>
   const formElement = document.getElementById('form');
+  const inputElement = document.getElementById('input');
   const resultElement = document.getElementById('result');
-  const urlElement = document.getElementById('url');
   const scoreElement = document.getElementById('score');
   const failedElement = document.getElementById('failed');
   const passedElement = document.getElementById('passed');
   const errorElement = document.getElementById('error');
+  let savedValue = '';
   formElement.addEventListener('submit', e => {
     e.preventDefault();
     const formData = new FormData(formElement);
@@ -44,9 +48,10 @@
     })
     .then(response => response.json())
     .then(result => {
+      savedValue = inputElement.value;
+      inputElement.value = result.url;
       errorElement.style.display = 'none';
       resultElement.style.display = 'block';
-      urlElement.innerHTML = result.url;
       scoreElement.innerHTML = parseInt(result.passed.length / (result.passed.length + result.failed.length) * 100, 10);
       failedElement.innerHTML = '';
       passedElement.innerHTML = '';
@@ -58,6 +63,7 @@
         }
       } else {
         const listItem = document.createElement('li');
+        listItem.classList.add('info');
         listItem.append(document.createTextNode('No tests failed'));
         failedElement.appendChild(listItem);
       }
@@ -69,11 +75,13 @@
         }
       } else {
         const listItem = document.createElement('li');
+        listItem.classList.add('info');
         listItem.append(document.createTextNode('No tests passed'));
         passedElement.appendChild(listItem);
       }
     })
     .catch(error => {
+      if (savedValue.length < 0) inputElement.value = savedValue;
       resultElement.style.display = 'none';
       errorElement.style.display = 'block';
       console.error(error);
