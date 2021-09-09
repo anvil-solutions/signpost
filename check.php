0729866f13482ce5b02f765e95ac72b754159ab9
Domi04151309 <36272047+Domi04151309@users.noreply.github.com>
2021-09-09 20:44:26 +0200

    Add positive and negative strings

--- check.php
+++ check.php
@@ -24,10 +24,10 @@
 
   $result = ['url' => $_POST['url'], 'passed' => [], 'failed' =>[]];
 
-  function addToResult($bool, $string) {
+  function addToResult($bool, $positiveString, $negativeString) {
     global $result;
-    if ($bool) array_push($result['passed'], $string);
-    else array_push($result['failed'], $string);
+    if ($bool) array_push($result['passed'], $positiveString);
+    else array_push($result['failed'], $negativeString);
   }
 
   //Enable text compression
@@ -35,16 +35,18 @@
   //Use HTTP/2
   addToResult(
     strpos($headers, 'HTTP/2') !== false,
-    'Use HTTP/2'
+    'Uses HTTP/2',
+    'Does Not use HTTP/2'
   );
 
   //Avoids document.write()
   addToResult(
     strpos($file, 'document.write(') === false,
-    'Avoids document.write()'
+    'Avoids document.write()',
+    'Uses document.write()'
   );
 
-  //<html> element does have a [lang] attribute
+  //<html> element has a [lang] attribute
   $passed = false;
   $nodeList = $doc->getElementsByTagName('html');
   foreach($nodeList as $node) {
@@ -53,7 +55,8 @@
   }
   addToResult(
     $passed,
-    '<html> element does have a [lang] attribute'
+    '<html> element has a [lang] attribute',
+    '<html> element does not have a [lang] attribute'
   );
 
   //[aria-hidden="true"] is not present on the document <body>
@@ -65,13 +68,15 @@
   }
   addToResult(
     $passed,
-    '[aria-hidden="true"] is not present on the document <body>'
+    '[aria-hidden="true"] is not present on the document <body>',
+    '[aria-hidden="true"] is present on the document <body>'
   );
 
   //Document has a <title> element
   addToResult(
     $doc->getElementsByTagName('title')->length > 0,
-    'Document has a <title> element'
+    'Document has a <title> element',
+    'Document does not have a <title> element'
   );
 
   //[user-scalable="no"] is not used in the <meta name="viewport"> element
@@ -87,7 +92,8 @@
   }
   addToResult(
     $passed,
-    '[user-scalable="no"] is not used in the <meta name="viewport"> element'
+    '[user-scalable="no"] is not used in the <meta name="viewport"> element',
+    '[user-scalable="no"] is used in the <meta name="viewport"> element'
   );
 
   //<frame> or <iframe> elements have a title
@@ -102,7 +108,8 @@
   }
   addToResult(
     $passed,
-    '<frame> or <iframe> elements have a title'
+    '<frame> or <iframe> elements have a title',
+    '<frame> or <iframe> elements do not have a title'
   );
 
   //Image elements have [alt] attributes
@@ -113,7 +120,8 @@
   }
   addToResult(
     $passed,
-    'Image elements have [alt] attributes'
+    'Image elements have [alt] attributes',
+    'Image elements do not have [alt] attributes'
   );
 
   //<input type="image"> elements have [alt] text
@@ -127,7 +135,8 @@
   }
   addToResult(
     $passed,
-    '<input type="image"> elements have [alt] text'
+    '<input type="image"> elements have [alt] text',
+    '<input type="image"> elements do not have [alt] text'
   );
 
   //Form elements have associated labels
@@ -147,7 +156,8 @@
   }
   addToResult(
     $passed,
-    'The document does not use <meta http-equiv="refresh">'
+    'The document does not use <meta http-equiv="refresh">',
+    'The document uses <meta http-equiv="refresh">'
   );
 
   //<object> elements have [alt] text
@@ -158,15 +168,17 @@
   }
   addToResult(
     $passed,
-    '<object> elements have [alt] text'
+    '<object> elements have [alt] text',
+    '<object> elements do not have [alt] text'
   );
 
   //<video> elements contain a <track> element with [kind="captions"]
 
-  //Does use HTTPS
+  //Uses HTTPS
   addToResult(
     strpos($_POST['url'], 'https://') !== false,
-    'Does use HTTPS'
+    'Uses HTTPS',
+    'Does not use HTTPS'
   );
 
   //Links to cross-origin destinations are safe
@@ -184,13 +196,15 @@
   }
   addToResult(
     $passed,
-    'Links to cross-origin destinations are safe'
+    'Links to cross-origin destinations are safe',
+    'Links to cross-origin destinations are not safe'
   );
 
   //Page has the HTML doctype
   addToResult(
     strpos($file, '<!DOCTYPE html>') !== false,
-    'Page has the HTML doctype'
+    'Page has the HTML doctype',
+    'Page does not have the HTML doctype'
   );
 
   //Properly defines charset
@@ -208,7 +222,8 @@
   }
   addToResult(
     $passed,
-    'Properly defines charset'
+    'Properly defines charset',
+    'Does not roperly define charset'
   );
 
   //Avoids Application Cache
@@ -224,7 +239,8 @@
   }
   addToResult(
     $passed,
-    'Avoids Application Cache'
+    'Avoids Application Cache',
+    'Uses Application Cache'
   );
 
   //Document has a meta description
@@ -238,7 +254,8 @@
   }
   addToResult(
     $passed,
-    'Document has a meta description'
+    'Document has a meta description',
+    'Document does not have a meta description'
   );
 
   //Has a <meta name="viewport"> tag with width or initial-scale
@@ -254,19 +271,22 @@
   }
   addToResult(
     $passed,
-    'Has a <meta name="viewport"> tag with width or initial-scale'
+    'Has a <meta name="viewport"> tag with width or initial-scale',
+    'Does not have a <meta name="viewport"> tag with width or initial-scale'
   );
 
-  //Page has successful HTTP status code
+  //Page has a successful HTTP status code
   addToResult(
     $http_status > 199 && $http_status < 300,
-    'Page has successful HTTP status code'
+    'Page has a successful HTTP status code',
+    'Page hdoes not have a successful HTTP status code'
   );
 
   //Document avoids plugins
   addToResult(
     strpos($file, '.swf') === false && strpos($file, '.flv') === false && strpos($file, '.class') === false && strpos($file, '.xap') === false,
-    'Document avoids plugins'
+    'Document avoids plugins',
+    'Document uses plugins'
   );
 
   echo json_encode($result);
